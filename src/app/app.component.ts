86fe54c076c8298b5595311da7b52f8b17d59dd3
Valery Yakovlev <vd.yakovlev@mpoffice.ru>
2018-11-18 17:29:23 +0300

    simple impl of questionnaire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+
 import * as questions from './questions.json';
 
 @Component({
@@ -8,17 +9,40 @@ import * as questions from './questions.json';
 })
 
 export class AppComponent {
-  answer: int;
+  showResult: boolean;
+  success: boolean;
+  answer: any;
   value: any;
   questionCnt = 0;
 
   constructor() {
     this.value = questions.default[0];
+    this.success = false;
+    this.showResult = false;
+  }
+
+  checkAnswer() {
+    this.showResult = true;
+
+    let givenAnswer = 0;
+    for (const entry of this.value.answers) {
+      if (entry === this.answer) {
+        break;
+      }
+      givenAnswer++;
+    }
+
+    this.success = this.value.correct_answer === givenAnswer;
   }
 
   nextQuestion() {
+    this.showResult = false;
+
+    this.questionCnt++;
+    if (this.questionCnt >= questions.default.length) {
+      return;
+    }
 
     this.value = questions.default[this.questionCnt];
-    this.questionCnt++;
   }
 }
